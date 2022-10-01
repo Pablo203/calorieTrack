bf8fd5b0ae616a0b44e743cd6297e176347aa269
Pablo203 <76450441+Pablo203@users.noreply.github.com>
2022-10-01 22:02:47 +0200

    Add rest of CRUD

--- userMenu/urls.py
+++ userMenu/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', views.menu, name='menu'),
@@ -8,8 +9,10 @@ urlpatterns = [
     path('newProduct/', views.addNewProduct, name='newProduct'),
     path('newProductExecute/', views.newProductExecute, name="newProductExecute"),
     path('productsList/', views.productsList, name="productList"),
+    path('product/<int:id>/', views.productDetail, name="productDetail"),
     path('<str:historyDate>/', views.historyDetail, name='historyDetail'),
     path('<str:meal>/', views.showProductsToAdd, name="showProd"),
     path('<str:meal>/<str:product_name>/', views.addProductToMeal, name='meal'),
-    
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- userMenu/views.py
+++ userMenu/views.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2.driver import Driver
 from xmlrpc.client import ResponseError
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
@@ -91,16 +92,35 @@ def newProductExecute(request):
 
 
 @api_view(['GET', 'POST'])
-def productsList(request):
+def productsList(request, format=None):
 
     if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
 
-        return JsonResponse({"drinks" :serializer.data})
+        return Response(serializer.data)
 
     if request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def productDetail(request, id, format=None):
+    try:
+        product = Product.objects.get(pk=id)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
