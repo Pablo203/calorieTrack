cd8edbf79cd9d34b20335d4e83a60de3d43967f2
Pablo203 <76450441+Pablo203@users.noreply.github.com>
2022-10-01 21:33:36 +0200

    Add REST API for getting Products list

--- userMenu/urls.py
+++ userMenu/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('history/', views.history, name="history"),
     path('newProduct/', views.addNewProduct, name='newProduct'),
     path('newProductExecute/', views.newProductExecute, name="newProductExecute"),
-    path('<str:historyDate>', views.historyDetail, name='historyDetail'),
+    path('productsList/', views.productsList, name="productList"),
+    path('<str:historyDate>/', views.historyDetail, name='historyDetail'),
     path('<str:meal>/', views.showProductsToAdd, name="showProd"),
-    path('<str:meal>/<str:product_name>/', views.addProductToMeal, name='meal')
+    path('<str:meal>/<str:product_name>/', views.addProductToMeal, name='meal'),
+    
 ]
--- userMenu/views.py
+++ userMenu/views.py
@@ -1,8 +1,14 @@
+from xmlrpc.client import ResponseError
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.urls import is_valid_path
 from .models import UserIntreface, MealList, Meals, History, Product
 from .forms import ProductForm
 import datetime
+from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 def menu(request):
@@ -14,7 +20,10 @@ def menu(request):
     totalEaten = meals.breakfast + meals.lunch + meals.dinner
     caloriesToSubstract = totalEaten - user.eaten
     user.eaten = totalEaten
-    user.toEat = user.toEat - caloriesToSubstract
+    if user.toEat - caloriesToSubstract < 0:
+        user.toEat = 0
+    else:
+        user.toEat = user.toEat - caloriesToSubstract
     user.save()
 
     return render(request, 'index.html', {'user': user, 'mealList': MealList, 'meals': meals})
@@ -46,9 +55,9 @@ def showProductsToAdd(request, meal):
     return render(request, 'addProduct.html', {'products': Product.objects.all(), 'meal': meal})   
 
 def addProductToMeal(request, meal, product_name):
-
     requestedProd = get_object_or_404(Product, name=product_name)
     mealToAdd = get_object_or_404(Meals, date=datetime.date.today())
+
     if meal == "breakfast":
         mealToAdd.breakfast = mealToAdd.breakfast + requestedProd.calories
     elif meal == "lunch":
@@ -62,7 +71,6 @@ def addProductToMeal(request, meal, product_name):
 
 
 def history(request):
-    #history = get_object_or_404(History)
     return render(request, 'history.html', {'history':History.objects.all()})
 
 def historyDetail(request, historyDate):
@@ -77,8 +85,22 @@ def newProductExecute(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            #productName = form.cleaned_data['name']
-            #calories = form.cleaned_data['calories']
             newProduct = Product.objects.create(name=form.cleaned_data['name'], calories=form.cleaned_data['calories'])
             newProduct.save()
     return HttpResponseRedirect("/calorie")
+
+
+@api_view(['GET', 'POST'])
+def productsList(request):
+
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return JsonResponse({"drinks" :serializer.data})
+
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
